d30b648613f2f13c45ce0066f75b25d23ba0c2d2
Nalu420 <199804231+Nalu420@users.noreply.github.com>
2025-07-02 08:53:20 +0900

    Grid修正（グラフとMatchForm）

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -9,47 +9,64 @@ import { useEffect, useState } from "react";
 const STORAGE_KEY = "matchResult";
 
 export default function DashboardPage() {
-	const [matches, setMatches] = useState([]); // MatchFormから追加される対戦履歴一覧（全体の戦績）
-	const [selectDeckId, setSelectDeckId ] = useState(null); // 現在選択中のデッキid（DeckPanelからリフトアップ)
+  const [matches, setMatches] = useState([]); // MatchFormから追加される対戦履歴一覧（全体の戦績）
+  const [selectDeckId, setSelectDeckId] = useState(null); // 現在選択中のデッキid（DeckPanelからリフトアップ)
 
   // 初回マウント時にローカルストレージから保存済みの戦績を読み込む
-	useEffect(() => {
-		const savedMatches = JSON.parse(localStorage.getItem(STORAGE_KEY) || "[]") // ローカルからmatchResultか空配列を取得
-		setMatches(savedMatches); // 再レンダリング時にもグラフが復元される
-	},[]);
+  useEffect(() => {
+    const savedMatches = JSON.parse(localStorage.getItem(STORAGE_KEY) || "[]"); // ローカルからmatchResultか空配列を取得
+    setMatches(savedMatches); // 再レンダリング時にもグラフが復元される
+  }, []);
 
-	// ローカルストレージから全体の戦績を取得してdeckIdを照らし合わせてsetMatchesで更新する
-	// propsとしてDeckPanelに関数をわたす
-	const handleDeckChange =(deckId) => {
-		const data = JSON.parse(localStorage.getItem("matchResult") || "[]");
-		const filtered = data.filter((m) => m.deckId === deckId);
-		setMatches(filtered)
-	}
+  // ローカルストレージから全体の戦績を取得してdeckIdを照らし合わせてsetMatchesで更新する
+  // propsとしてDeckPanelに関数をわたす
+  const handleDeckChange = (deckId) => {
+    const data = JSON.parse(localStorage.getItem("matchResult") || "[]");
+    const filtered = data.filter((m) => m.deckId === deckId);
+    setMatches(filtered);
+  };
   return (
-		<Grid container spacing={0.5}>
-			{/* デッキの選択・作成・削除するフォーム */}
-			{/* DeckPanelとMatchFormにpropsとしてselectDeckIdを渡す */}
-			<Grid size={4}><DeckPanel selectDeckId={selectDeckId} onSelectDeck={setSelectDeckId} onDeckChange={handleDeckChange} /></Grid>
+    <Grid container columns={12} sx={{ width:"100%" }} spacing={0.5}>
+      {/* デッキの選択・作成・削除するフォーム */}
+      {/* DeckPanelとMatchFormにpropsとしてselectDeckIdを渡す */}
+      <Grid size={{ xs: 12, sm: 6, md:4 }}>
+        <DeckPanel
+          selectDeckId={selectDeckId}
+          onSelectDeck={setSelectDeckId}
+          onDeckChange={handleDeckChange}
+        />
+      </Grid>
 
-			{/* グラフを表示するフォーム*/}
-			{/* MatchFormの登録内容(matches)を元に勝率グラフを更新 */}
-			<Grid item xs={12} md={4}>
-			{selectDeckId ? (
-						<WinRateGraph matches={matches}/> ): (
-						<Box>
-							{/* １つ目のTypographyはコンポーネント化するかもしれない */}
-							<Typography
-											variant="h6"
-											component="h2"
-											sx={{ bgcolor: "grey.300", p: 2 }}>グラフ</Typography>
-										<Typography sx={{ p: 2}}>デッキを選択すると戦績が表示されます</Typography>
-						</Box>
-				)}
-			</Grid>
+      {/* グラフを表示するフォーム*/}
+      {/* MatchFormの登録内容(matches)を元に勝率グラフを更新 */}
+      <Grid size={{ xs: 12, sm: 6, md: 4 }}>
+        {selectDeckId ? (
+          <WinRateGraph matches={matches} />
+        ) : (
+          <Box>
+            {/* １つ目のTypographyはコンポーネント化するかもしれない */}
+            <Typography
+              variant="h6"
+              component="h2"
+              sx={{ bgcolor: "grey.300", p: 2 }}
+            >
+              グラフ
+            </Typography>
+            <Typography sx={{ p: 2 }}>
+              デッキを選択すると戦績が表示されます
+            </Typography>
+          </Box>
+        )}
+      </Grid>
 
-			{/* 戦績（matches）を新規登録するフォーム*/}
-			{/* selectDeckIdで選択中のデッキの戦績に限定できる */}
-			<Grid size={4}><MatchForm selectDeckId={selectDeckId} onAddMatch={(newMatch) => setMatches((prev) => [...prev, newMatch])} /></Grid>
-		</Grid>
+      {/* 戦績（matches）を新規登録するフォーム*/}
+      {/* selectDeckIdで選択中のデッキの戦績に限定できる */}
+      <Grid size={{ xs: 12, sm: 6, md: 4 }}>
+        <MatchForm
+          selectDeckId={selectDeckId}
+          onAddMatch={(newMatch) => setMatches((prev) => [...prev, newMatch])}
+        />
+      </Grid>
+    </Grid>
   );
 }
