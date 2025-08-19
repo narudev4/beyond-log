9b3808002daced1b5d1d5b074e4c38cf69efab0a
Nalu420 <199804231+Nalu420@users.noreply.github.com>
2025-08-19 17:38:57 +0900

    feat(types): domain型を追加

---
+++ src/types/domain.ts
@@ -0,0 +1,26 @@
+export type ClassName =
+  | "エルフ"
+  | "ロイヤル"
+  | "ウィッチ"
+  | "ドラゴン"
+  | "ナイトメア"
+  | "ビショップ"
+  | "ネメシス";
+
+export type Result = "win" | "lose";
+
+export interface Deck {
+  id: string;
+  name: string;
+  class: ClassName;
+}
+
+export interface Match {
+  id: string;
+  deckId: string;
+  opponentDeck: ClassName;
+  wentFirst: boolean;
+  result: Result;
+  memo?: string;
+  date: string;
+}
